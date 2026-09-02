0fc1740af8ac511726888a2035874c06b58be7ed
mario <mqa010225@gmail.com>
2026-09-02 21:17:15 +0800

    fix(platforms): include portal in gateway cors policy

--- scripts/unified-identity-local.sh
+++ scripts/unified-identity-local.sh
@@ -1818,7 +1818,7 @@ publish_gateway_routes() {
   fi
 
   cors_policy_id=unified-local-cors
-  cors_origins='["http://127.0.0.1:18121","http://127.0.0.1:18131","http://127.0.0.1:18141","http://127.0.0.1:18152"]'
+  cors_origins='["http://127.0.0.1:18121","http://127.0.0.1:18125","http://127.0.0.1:18131","http://127.0.0.1:18141","http://127.0.0.1:18152"]'
   cors_methods='["GET","POST","PUT","PATCH","DELETE","OPTIONS"]'
   cors_headers='["Authorization","Content-Type","X-IDP-CSRF","X-CSRF-TOKEN","Idempotency-Key"]'
   cors_exposed='["traceparent","x-egon-request-id"]'
